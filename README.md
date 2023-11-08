da72899f5f0980b6c59fd3a419c8644f1613d5c8
Vladislav Maraev <vlad@talkamatic.se>
2023-11-08 13:52:51 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# [Lab 1](./lab1.org)
+
+# [Lab 2](./lab2.org)
+
+# [Lab 3](./lab3.org)
+
+
+# [Getting started](./README.org)
---
+++ src/index.ts
@@ -0,0 +1,172 @@
+import { createMachine, createActor, assign } from "xstate";
+import { speechstate, Settings, Hypothesis } from "speechstate";
+
+const azureCredentials = {
+  endpoint:
+    "https://northeurope.api.cognitive.microsoft.com/sts/v1.0/issuetoken",
+  key: "",
+};
+
+const settings: Settings = {
+  azureCredentials: azureCredentials,
+  asrDefaultCompleteTimeout: 0,
+  locale: "en-US",
+  asrDefaultNoInputTimeout: 5000,
+  ttsDefaultVoice: "en-GB-RyanNeural",
+};
+
+interface DMContext {
+  spstRef?: any;
+  lastResult?: Hypothesis[];
+}
+
+// helper functions
+const say =
+  (text: string) =>
+  ({ context }) => {
+    context.spstRef.send({
+      type: "SPEAK",
+      value: { utterance: text },
+    });
+  };
+const listen =
+  () =>
+  ({ context }) =>
+    context.spstRef.send({
+      type: "LISTEN",
+    });
+
+// machine
+const dmMachine = createMachine(
+  {
+    id: "root",
+    type: "parallel",
+    states: {
+      DialogueManager: {
+        initial: "Prepare",
+        states: {
+          Prepare: {
+            on: { ASRTTS_READY: "Ready" },
+            entry: [
+              assign({
+                spstRef: ({ spawn }) => {
+                  return spawn(speechstate, {
+                    input: {
+                      settings: settings,
+                    },
+                  });
+                },
+              }),
+            ],
+          },
+          Ready: {
+            initial: "Greeting",
+            states: {
+              Greeting: {
+                entry: "speak.greeting",
+                on: { SPEAK_COMPLETE: "HowCanIHelp" },
+              },
+              HowCanIHelp: {
+                entry: say("You can say whatever you like."),
+                on: { SPEAK_COMPLETE: "Ask" },
+              },
+              Ask: {
+                entry: listen(),
+                on: {
+                  RECOGNISED: {
+                    target: "Repeat",
+                    actions: [
+                      ({ event }) => console.log(event),
+                      assign({
+                        lastResult: ({ event }) => event.value,
+                      }),
+                    ],
+                  },
+                },
+              },
+              Repeat: {
+                entry: ({ context }) => {
+                  context.spstRef.send({
+                    type: "SPEAK",
+                    value: { utterance: context.lastResult[0].utterance },
+                  });
+                },
+                on: { SPEAK_COMPLETE: "Ask" },
+              },
+              IdleEnd: {},
+            },
+          },
+        },
+      },
+      GUI: {
+        initial: "PageLoaded",
+        states: {
+          PageLoaded: {
+            entry: "gui.PageLoaded",
+            on: { CLICK: { target: "Inactive", actions: "prepare" } },
+          },
+          Inactive: { entry: "gui.Inactive", on: { ASRTTS_READY: "Active" } },
+          Active: {
+            initial: "Idle",
+            states: {
+              Idle: {
+                entry: "gui.Idle",
+                on: { TTS_STARTED: "Speaking", ASR_STARTED: "Listening" },
+              },
+              Speaking: {
+                entry: "gui.Speaking",
+                on: { SPEAK_COMPLETE: "Idle" },
+              },
+              Listening: { entry: "gui.Listening", on: { RECOGNISED: "Idle" } },
+            },
+          },
+        },
+      },
+    },
+  },
+  {
+    // custom actions
+    //
+    actions: {
+      prepare: ({ context }) =>
+        context.spstRef.send({
+          type: "PREPARE",
+        }),
+      // saveLastResult:
+      "speak.greeting": ({ context }) => {
+        context.spstRef.send({
+          type: "SPEAK",
+          value: { utterance: "Hello world!" },
+        });
+      },
+      "speak.how-can-I-help": ({ context }) =>
+        context.spstRef.send({
+          type: "SPEAK",
+          value: { utterance: "How can I help you?" },
+        }),
+      "gui.PageLoaded": ({}) => {
+        document.getElementById("button").innerText = "Click to start!";
+      },
+      "gui.Inactive": ({}) => {
+        document.getElementById("button").innerText = "Inactive";
+      },
+      "gui.Idle": ({}) => {
+        document.getElementById("button").innerText = "Idle";
+      },
+      "gui.Speaking": ({}) => {
+        document.getElementById("button").innerText = "Speaking...";
+      },
+      "gui.Listening": ({}) => {
+        document.getElementById("button").innerText = "Listening...";
+      },
+    },
+  },
+);
+
+const actor = createActor(dmMachine).start();
+
+document.getElementById("button").onclick = () => actor.send({ type: "CLICK" });
+
+actor.subscribe((state) => {
+  console.log(state.value);
+});
